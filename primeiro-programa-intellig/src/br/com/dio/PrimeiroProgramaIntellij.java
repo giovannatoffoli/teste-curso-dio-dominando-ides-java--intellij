cfb49eb0d3ec7b4c005bef2aa897187ef5c81c8d
giovannatoffoli <toffoli.giovannalima@gmail.com>
2022-07-20 21:10:46 -0300

    Criando classe Livro

--- primeiro-programa-intellig/src/br/com/dio/PrimeiroProgramaIntellij.java
+++ primeiro-programa-intellig/src/br/com/dio/PrimeiroProgramaIntellij.java
@@ -7,9 +7,10 @@ public class PrimeiroProgramaIntellij {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
-
         System.out.println(gato);
 
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro);
 /*
         int a = 5;
         int b = 3;
@@ -18,4 +19,37 @@ public class PrimeiroProgramaIntellij {
 */
     }
 
+    static class Livro {
+        private String nome;
+        private Integer numPaginas;
+
+        public Livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas=" + numPaginas +
+                    '}';
+        }
+    }
 }
